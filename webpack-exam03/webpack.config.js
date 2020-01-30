bce51972dc68c4c67d23173e6b0747344c4056ea
SC <topsky2008@gmail.com>
2020-01-30 19:32:26 +0900

    learn webpack ( webpack handbook ) plugins

--- webpack-exam03/webpack.config.js
+++ webpack-exam03/webpack.config.js
@@ -1,31 +1,21 @@
-var webpack = require('webpack');
 var path = require('path');
-var ManifestPlugin = require('webpack-manifest-plugin');
-
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-    entry: {
-        main: './app/index.js',
-        vendor: [
-            'moment',
-            'lodash'
-        ]
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        // webpack 3.x 에서만 가능
-        // webpack 4.x 이상부턴 SplitChunksPlugin
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     name: 'vendor' // Specify the common bundle's name.
-        // }),
-        new webpack.optimize.CommonsChunkPlugin({
-            names: ['vendor', 'manifest'] // Extract the webpack bootstrap logic into manifest.js
-        }),
-        new ManifestPlugin({
-            fileName: 'manifest.json',
-            basePath: './dist/'
-        })
-    ],
+  mode: 'none',
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{loader : MiniCssExtractPlugin.loader}, 'css-loader']
+      }
+    ]
+  },
+  plugins : [
+      new MiniCssExtractPlugin()
+  ],
 }
