205650643af1fdfe0ec438d0fd53b2220c53deaa
SC <topsky2008@gmail.com>
2020-02-02 15:49:14 +0900

    learn webpack

--- webpack-exam06/src/index.js
+++ webpack-exam06/src/index.js
@@ -1,12 +1,21 @@
-function getComponent() {
-    return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-        const element = document.createElement('div');
 
-        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-        return element;
+// function getComponent() {
+//     return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
+//         const element = document.createElement('div');
 
-    }).catch(error => 'An error occurred while loading the component');
+//         element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//         return element;
+
+//     }).catch(error => 'An error occurred while loading the component');
+// }
+async function getComponent() {
+    const element = document.createElement('div');
+    const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
+
+    element.innerHTML = _.join(['Hello','webpack'],' ');
+    return element;
 }
 
 getComponent().then(component=>{
